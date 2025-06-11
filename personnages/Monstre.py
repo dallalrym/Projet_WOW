384a35ba601100a0c3649aa70f4581c642f54ce7
Gildas29 <gildas.couillard@viacesi.fr>
2025-06-11 15:01:15 +0200

    Update Monstre.py

--- personnages/Monstre.py
+++ personnages/Monstre.py
@@ -1,5 +1,5 @@
 from personnages.Personnage import Personnage
 
 class Monstre(Personnage):
-    def __init__(self, nom, points_de_vie, arme):
-        super().__init__(nom, points_de_vie, arme)
+    def __init__(self, nom, points_de_vie, endu, force, sacoche, arme_equipee=None, bouclier_equipe=None):
+        super().__init__(nom, points_de_vie, endu, force, sacoche, arme_equipee, bouclier_equipe)
